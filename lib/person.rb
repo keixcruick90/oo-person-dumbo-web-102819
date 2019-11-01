75571bb164c2106d699f440f7a6983fa100d51d0
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-11-01 10:40:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,75 @@
-# your code goes here
+class Person
+  attr_accessor :bank_account
+  attr_reader :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def name
+    @name
+  end
+
+  def happiness=(happiness_number)
+    @happiness = happiness_number
+    @happiness = 10 if happiness > 10
+    @happiness = 0 if happiness < 0
+  end
+
+  def hygiene=(hygiene_number)
+    @hygiene = hygiene_number
+    @hygiene = 10 if hygiene > 10
+    @hygiene = 0 if hygiene < 0
+  end
+
+  def happy?
+    if happiness > 7
+      return true
+    else
+      return false
+    end
+  end
+
+  def clean?
+    if hygiene> 7
+      return true
+    else
+      return false
+    end
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    return 'all about the benjamins'
+  end
+
+  def take_bath
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(Felix)
+    @Felix = Felix
+    self.happiness += 3
+    Felix.happiness += 3
+    return "Hi Felix! It's Stella. How are you?"
+  end
+
+  def start_conversation
+
+  end
+
+end
+
+Felix = Person.new("Felix")
+Felix = Person.name
+Felix
