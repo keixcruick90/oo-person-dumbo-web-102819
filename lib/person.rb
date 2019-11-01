dc30ed7dcd713f9ae00de9b7edaf37ced8c4cbb0
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-11-01 11:22:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -62,7 +62,7 @@ class Person
     return "Hi Felix! It's Stella. How are you?"
   end
 
-  def start_conversation(topic)
+  def start_conversation
     @topic = topic
     topic = ["politic", "weather"]
     if topic == "politics"
